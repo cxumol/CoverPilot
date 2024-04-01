0f7e26fb465605407a220789530d2ea1d8b4e269
cxumol <cxumol@gmail.com>
2024-04-01 02:56:33 +0000

    update dir

--- .litng.gradio.sh
+++ .litng.gradio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pip install -U "gradio>=4,<=5"
-GRADIO_SERVER_PORT=7860 gradio app/app.py --demo-name app --watch-dirs .
+GRADIO_SERVER_PORT=7860 gradio app/app.py --demo-name app --watch-dirs app
